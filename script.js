a444a87953adccadfda207782b0b493a36445dc1
WCiovh <wiktor.c175@gmail.com>
2025-10-28 09:57:05 +0000

    2_1.1

--- script.js
+++ script.js
@@ -15,22 +15,20 @@
   })
 
   cw1.addEventListener("click", function () {
-    cw1.addEventListener("click", function () {
-      fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json())
-        .then(posts => {
-          console.log(posts)
-          let html = '<h2>Lista postów:</h2><ul>';
-          posts.forEach(post => {
-            html += `<li>
-              <strong>Post ${post.id}: ${post.title}</strong><br>
-              ${post.body}
-            </li>`;
-          });
-          html += '</ul>';
-          answer.innerHTML = html;
-        })
-    })
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(posts => {
+        console.log(posts)
+        let html = '<h2>Lista postów:</h2><ul>';
+        posts.forEach(post => {
+          html += `<li>
+            <strong>Post ${post.id}: ${post.title}</strong><br>
+            ${post.body}
+          </li>`;
+        });
+        html += '</ul>';
+        answer.innerHTML = html;
+      })
   })
 
   cw2.addEventListener("click", function () {
